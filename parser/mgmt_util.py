5bf4564ced15a5ecfa22678e97b7a3aba6ed382a
Umarrrrrrrrrrrrrrrr <np05cp4a230043@iic.edu.np>
2025-08-05 15:54:21 +0545

    adding mgmt_utils.py

---
+++ parser/mgmt_util.py
@@ -0,0 +1,15 @@
+import os
+import pdfplumber
+
+def load_resumes_and_labels(folder_path):
+    texts, labels = [], []
+    for files in os.listdir(folder_path):
+        if file.endswith('.pdf'):
+            with pdfplumber.open(os.path.join(folder_path, file)) as pdf:
+                text = ''
+                for page in pdf.pages:
+                    text += page.extract_text() or ''
+                texts.append(text)
+                score = int(file.split('_')[-1].replace('.pdf', ''))
+                labels.append(score)
+    return texts, labels
