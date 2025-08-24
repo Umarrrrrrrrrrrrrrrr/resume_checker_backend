e3869f8d55c2d8fd2be94c1109b997bc49f93df5
Umarrrrrrrrrrrrrrrr <np05cp4a230043@iic.edu.np>
2025-08-24 19:57:17 +0545

    modified

--- dotcom/apps.py
+++ dotcom/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DoorConfig(AppConfig):
+class DotcomConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'door'
+    name = 'dotcom'
